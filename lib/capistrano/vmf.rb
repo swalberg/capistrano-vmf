bc21a4bf88adffb7f186a6953c704160d3aff436
Sean Walberg <sean@ertw.com>
2014-03-14 21:54:16 -0500

    Fix up includes so rake tasks work, too

--- lib/capistrano/vmf.rb
+++ lib/capistrano/vmf.rb
@@ -1,2 +1,6 @@
-load File.expand_path('../tasks/vmf.cap', __FILE__)
+require 'capistrano/all'
+require 'capistrano/setup'
+require 'capistrano/deploy'
+
 require 'rvm1/capistrano3'
+load File.expand_path('../tasks/vmf.cap', __FILE__)
