45fae0b416c46abed7d670d8828043019a7e5d0d
Sean Walberg <sean@ertw.com>
2014-03-19 19:44:13 -0500

    Redis and files tasks

--- lib/capistrano/vmf.rb
+++ lib/capistrano/vmf.rb
@@ -4,3 +4,5 @@ require 'capistrano/deploy'
 
 require 'rvm1/capistrano3'
 load File.expand_path('../tasks/vmf.cap', __FILE__)
+load File.expand_path('../tasks/redis.cap', __FILE__)
+load File.expand_path('../tasks/files.cap', __FILE__)
